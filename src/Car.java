54dc37ebeed2bf6554866d940fd75e3fd8c5145e
Gustaf Backman <g1q2b@ugrad.cs.ubc.ca>
2019-01-31 13:12:06 +0100

    .java

--- src/Car.java
+++ src/Car.java
@@ -1,11 +1,13 @@
 public class Car {
-    private String registrationNumber = "";
-    private String model = "";
+    private String registrationNumber;
+    private String model;
     private double speed = 0.0;
+    private static int numberOfCars;
 
     public Car(String number, String carModel) {
         registrationNumber = number;
         model = carModel;
+        numberOfCars++;
     }
 
     public void setSpeed(double newSpeed){
@@ -14,5 +16,7 @@ public class Car {
     public double getSpeed() {
         return speed;
     }
-
+    public static int getNumberOfCars() {
+        return numberOfCars;
+    }
 }
