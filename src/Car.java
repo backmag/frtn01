29d782f88f354f5dbd28fcab58883141e11e1060
Gustaf Backman <g1q2b@ugrad.cs.ubc.ca>
2019-01-31 13:30:15 +0100

    Added Vehicle and Truck

--- src/Car.java
+++ src/Car.java
@@ -1,22 +1,14 @@
-public class Car {
-    private String registrationNumber;
-    private String model;
-    private double speed = 0.0;
-    private static int numberOfCars;
+public class Car extends Vehicle {
+    private int numberOfPassengers;
 
     public Car(String number, String carModel) {
-        registrationNumber = number;
-        model = carModel;
-        numberOfCars++;
+        super(number,carModel);
     }
 
-    public void setSpeed(double newSpeed){
-        speed = newSpeed;
+    public int getNumberOfPassengers() {
+        return numberOfPassengers;
     }
-    public double getSpeed() {
-        return speed;
-    }
-    public static int getNumberOfCars() {
-        return numberOfCars;
+    public void setNumberOfPassengers(int passengers) {
+        numberOfPassengers = passengers;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,9 @@ public class Main {
         car1 = new Car("ABC111", "Volvo V70");
         car2 = new Car("DEF222", "Saab 9-5");
         car1.setSpeed(100);
+        car2.setSpeed(200);
         System.out.println("The speed of car1 is " + car1.getSpeed());
-        System.out.println("There are " + Car.getNumberOfCars() + " cars.");
+        System.out.println("There are " + Vehicle.getNumberOfVehicles() + " cars.");
+        Vehicle.printSpeed(car2);
     }
 }
